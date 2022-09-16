8223f9c710fd244eb4857ff0e9e49eed943709ef
dillon lee <dillonlee@tamu.edu>
2022-09-15 22:03:29 -0500

    Added unit tests to cover new attributes of book

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -9,4 +9,29 @@ RSpec.describe 'Creating a book', type: :feature do
     visit books_path
     expect(page).to have_content('harry potter')
   end
+
+  scenario 'valid inputs' do
+    visit new_book_path
+    fill_in 'Author', with: 'JK Rowling'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('JK Rowling')
+  end
+
+  scenario 'valid inputs' do
+    visit new_book_path
+    fill_in 'Price', with: '26.95'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content(26.95)
+  end
+
+  scenario 'valid inputs' do
+    visit new_book_path
+    fill_in 'published_date', with: '2019-01-13'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('2019-01-13')
+  end
 end
+
--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -15,3 +15,48 @@ RSpec.describe Book, type: :model do
     expect(subject).not_to be_valid
   end
 end
+
+RSpec.describe Book, type: :model do
+  subject do
+    described_class.new(author: 'jk rowling')
+  end
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without a name' do
+    subject.author = nil
+    expect(subject).not_to be_valid
+  end
+end
+
+RSpec.describe Book, type: :model do
+  subject do
+    described_class.new(price: 26.95)
+  end
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without a name' do
+    subject.price1 = nil
+    expect(subject).not_to be_valid
+  end
+end
+
+RSpec.describe Book, type: :model do
+  subject do
+    described_class.new(published_date: '2019-01-13')
+  end
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without a name' do
+    subject.published_date = nil
+    expect(subject).not_to be_valid
+  end
+end
